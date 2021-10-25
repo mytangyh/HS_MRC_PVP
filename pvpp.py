c603f54e2e7398a50fc74b23aa705d09dadce1fb
ioxv <88079004+ioxv@users.noreply.github.com>
2021-10-26 00:01:29 +0800

    Add files via upload

---
+++ pvpp.py
@@ -0,0 +1,114 @@
+# encoding: UTF-8
+# refer: https://nga.178.com/read.php?tid=11838545
+# python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyautogui Pillow
+# python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pymouse PyUserinput pypiwin32
+
+import re
+import pyautogui
+import time
+import win32gui
+from winguiauto import findTopWindow
+import os
+
+global LOGFILE, encoding, m, zoom
+pyautogui.FAILSAFE = False
+LOGFILE = "C:\\Program Files (x86)\\Hearthstone\\Logs\\Power.log"
+encoding = "UTF-8"
+zoom = 1    # Windows system zoom
+
+
+
+def findHS():
+    hwnd = findTopWindow("炉石传说")
+    rect = win32gui.GetWindowPlacement(hwnd)[-1]
+    return rect
+
+
+def movClick(x, y, t=0.7, zoom = zoom):
+    #pyautogui.moveTo(x, y, duration=t, tween=pyautogui.easeInOutQuad)
+    #time.sleep(0.3)
+    #pyautogui.click()
+    #time.sleep(0.233)
+    rect = findHS()
+    pyautogui.click(int(x * zoom + rect[0]),int(y * zoom + rect[1]))
+    time.sleep(t)
+
+
+def gaming(flag=0):
+    flagGameover = 0
+    flagTimesleep = 0
+    with open(LOGFILE, 'r', encoding=encoding) as log:
+        while log.readline():
+            pass
+        while True:
+            line = log.readline()
+            if line:
+                print(line)
+                flagTimesleep = 0
+                if re.findall(r"tag=STEP value=MAIN_ACTION", line):
+                    time.sleep(10)    # delay
+                    pyautogui.press('esc')
+                    time.sleep(1)
+                    movClick(278, 171)
+                    movClick(514, 242)
+                    movClick(514, 242)
+                    logLine = log.readline()
+                    while logLine:
+                        if re.findall(r"FINAL_GAMEOVER", logLine):
+                            flagGameover += 1
+                        logLine = log.readline()
+                    continue
+                elif re.findall(r"FINAL_GAMEOVER", line):
+                    time.sleep(5)
+                    logLine = log.readline()
+                    while logLine:
+                        if re.findall(r"FINAL_GAMEOVER", logLine):
+                            flagGameover += 1
+                        logLine = log.readline()
+                    flagGameover += 1
+            else:
+                time.sleep(1)
+                flagTimesleep += 1
+                print(flagTimesleep)
+            if flagGameover >= 4:
+                return
+            if flagTimesleep >= 123:                
+                movClick(289, 151)
+                movClick(289, 151)
+                movClick(157, 199)
+                movClick(157, 199)
+                movClick(192, 354)
+                movClick(192, 354)
+                movClick(382, 363)
+                movClick(382, 363)
+                movClick(427, 204)
+                movClick(427, 204)
+                loc = findHS()
+                pyautogui.screenshot(f"./hs/pvp/{time.time()}.png",region=(loc[0],loc[1],548*zoom,439*zoom))
+                movClick(284, 243)
+                movClick(284, 243)
+                return
+                    
+
+
+if __name__ == '__main__':
+    print(time.time())
+    try:
+        os.mkdir("./hs")
+        os.mkdir("./hs/pvp")
+    except:
+        pass
+    while True:
+        try:
+            movClick(443, 368)
+            movClick(443, 368)
+            movClick(443, 368)
+            movClick(443, 368)
+            gaming()
+            movClick(514, 242)
+            movClick(514, 242)
+            movClick(514, 242)
+            movClick(514, 242)
+            print("done")
+        except FileNotFoundError:
+            time.sleep(0.233)
